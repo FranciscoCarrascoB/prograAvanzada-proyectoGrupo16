759353a8ef1a4db6ceb5a42d181c87b0f2badc94
FranciscoCarrascoB <francisco.carrasco.b@mail.pucv.cl>
2025-08-25 04:49:08 -0400

    Cambios en main y creacion de clases

--- Main.java
+++ Main.java
@@ -1,6 +1,24 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
 public class Main {
-    public static void main(String[] args) {
-        
+    public static void main(String[] args) throws IOException {
+        String opcion;
+        BufferedReader lector = new BufferedReader(new InputStreamReader(System.in));
+
+        System.out.println("Bienvenido al sistema");
+        System.out.println("Que desea hacer?");
+        System.out.println("1.Agregar elementos");
+        System.out.println("2.Mostrar lista");
+        opcion = lector.readLine();
+
+        if(opcion == "1"){
+
+        }
+        else if(opcion == "2"){
+
+        }
     }
 }
 
